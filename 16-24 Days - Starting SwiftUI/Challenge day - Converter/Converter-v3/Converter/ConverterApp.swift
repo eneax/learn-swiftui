df9406e1917485ffdad29bc9fa7fa1d276e158dd
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-01-27 13:28:13 +0100

    add converter app

---
+++ 16-24 Days - Starting SwiftUI/Challenge day - Converter/Converter-v3/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Enea Xharja on 27.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
