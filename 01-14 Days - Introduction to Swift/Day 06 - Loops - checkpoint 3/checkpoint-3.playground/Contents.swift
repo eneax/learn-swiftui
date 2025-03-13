66836a5370d0e074ef4c6591b6e706dd72c336c7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-03-13 15:24:44 +0100

    Add Swift playground for Day 06: checkpoint 3 - FizzBuzz

---
+++ 01-14 Days - Introduction to Swift/Day 06 - Loops - checkpoint 3/checkpoint-3.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+// Checkpoint - FizzBuzz
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) {
+        if i.isMultiple(of: 5) {
+            print("FizzBuzz")
+        } else {
+            print("Fizz")
+        }
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
