d06a0424d6629b8739c979b28db66f9d1adaadff
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-03-26 10:25:47 +0100

    Add Swift app for Day 19: implement unit converter app

---
+++ 16-24 Days - Starting SwiftUI/Day 19 - Challenge day/Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Enea Xharja on 25.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
