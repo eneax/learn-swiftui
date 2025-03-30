deb52c2f115874e419dc9de9477e00fa800bdb2d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-03-30 22:14:44 +0200

    Add CustomModifiers project for Day 23

---
+++ 16-24 Days - Starting SwiftUI/Day 23 - Project 3 (part one)/CustomModifiers/CustomModifiers/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  CustomModifiers
+//
+//  Created by Enea Xharja on 30.03.25.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            // .modifier(Title())
+            .titleStyle()
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 16-24 Days - Starting SwiftUI/Day 23 - Project 3 (part one)/CustomModifiers/CustomModifiers/CustomModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifiersApp.swift
+//  CustomModifiers
+//
+//  Created by Enea Xharja on 30.03.25.
+//
+
+import SwiftUI
+
+@main
+struct CustomModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
