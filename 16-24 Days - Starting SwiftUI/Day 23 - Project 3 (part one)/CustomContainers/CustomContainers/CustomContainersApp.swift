7f246ae6c3c51b1e26e1ea5d9013bbce0803a75f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-03-30 22:15:02 +0200

    Add CustomContainers project for Day 23

---
+++ 16-24 Days - Starting SwiftUI/Day 23 - Project 3 (part one)/CustomContainers/CustomContainers/CustomContainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomContainersApp.swift
+//  CustomContainers
+//
+//  Created by Enea Xharja on 30.03.25.
+//
+
+import SwiftUI
+
+@main
+struct CustomContainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
