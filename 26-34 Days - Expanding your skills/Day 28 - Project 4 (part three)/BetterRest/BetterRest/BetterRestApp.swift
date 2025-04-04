c8eda643f12776565bb9c8d730a42d1b41cd8506
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-04 14:53:58 +0200

    Add BetterRest project for Day 28 with SleepCalculator model and UI

---
+++ 26-34 Days - Expanding your skills/Day 28 - Project 4 (part three)/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Enea Xharja on 03.04.25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
