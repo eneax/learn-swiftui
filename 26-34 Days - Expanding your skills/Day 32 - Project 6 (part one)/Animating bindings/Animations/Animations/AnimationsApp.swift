787786dbd78a2bc6a03d28a7150b48fa8837c803
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-08 09:26:09 +0200

    Add Animating bindings project for Day 32

---
+++ 26-34 Days - Expanding your skills/Day 32 - Project 6 (part one)/Animating bindings/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Enea Xharja on 07.04.25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 26-34 Days - Expanding your skills/Day 32 - Project 6 (part one)/Animating bindings/Animations/Animations/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Enea Xharja on 07.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        print(animationAmount)
+       
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
