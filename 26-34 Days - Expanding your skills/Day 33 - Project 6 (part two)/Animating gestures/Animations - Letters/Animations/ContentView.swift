cd2efaf29f8ee8bbaa2c780c78d78737392a5a55
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-09 09:43:03 +0200

    Add Animations - Letters project for Day 33

---
+++ 26-34 Days - Expanding your skills/Day 33 - Project 6 (part two)/Animating gestures/Animations - Letters/Animations/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Enea Xharja on 08.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { index in
+                Text(String(letters[index]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(index) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
