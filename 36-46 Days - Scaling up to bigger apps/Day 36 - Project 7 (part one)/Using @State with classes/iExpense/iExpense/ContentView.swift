7e1491a31884f8227f9c13fb7a7dffdc1149843c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-12 13:27:50 +0200

    Add Using @State with classes project

---
+++ 36-46 Days - Scaling up to bigger apps/Day 36 - Project 7 (part one)/Using @State with classes/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Enea Xharja on 12.04.25.
+//
+
+import SwiftUI
+
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
