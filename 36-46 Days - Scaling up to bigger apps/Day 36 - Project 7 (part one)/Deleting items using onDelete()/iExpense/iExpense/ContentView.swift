2cdecba63c7fb77cecb77972128f606c75417a97
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-12 13:30:25 +0200

    Add Deleting items using onDelete()

---
+++ 36-46 Days - Scaling up to bigger apps/Day 36 - Project 7 (part one)/Deleting items using onDelete()/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Enea Xharja on 12.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
