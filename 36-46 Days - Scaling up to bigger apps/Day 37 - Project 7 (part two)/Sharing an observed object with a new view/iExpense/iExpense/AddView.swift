6e6b49b8d1ccc1585f6cb9ae6fd1d86c8818c5d0
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-13 12:37:34 +0200

    Add Sharing an observed object with a new view

---
+++ 36-46 Days - Scaling up to bigger apps/Day 37 - Project 7 (part two)/Sharing an observed object with a new view/iExpense/iExpense/AddView.swift
@@ -0,0 +1,40 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Enea Xharja on 13.04.25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    let types = ["Personal", "Business"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "EUR"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
