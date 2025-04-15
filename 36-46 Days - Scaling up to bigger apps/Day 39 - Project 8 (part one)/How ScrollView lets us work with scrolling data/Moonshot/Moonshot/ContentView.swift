74f134ddfca0c98692135d0ef23bc19ce294a5d9
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-15 21:36:07 +0200

    Add How ScrollView lets us work with scrolling data

---
+++ 36-46 Days - Scaling up to bigger apps/Day 39 - Project 8 (part one)/How ScrollView lets us work with scrolling data/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Enea Xharja on 14.04.25.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Create a new CustomText")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText(text: "Item \($0)")
+                        .font(.title)
+                }
+            }
+        }
+        
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText(text: "Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 36-46 Days - Scaling up to bigger apps/Day 39 - Project 8 (part one)/How ScrollView lets us work with scrolling data/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Enea Xharja on 14.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
