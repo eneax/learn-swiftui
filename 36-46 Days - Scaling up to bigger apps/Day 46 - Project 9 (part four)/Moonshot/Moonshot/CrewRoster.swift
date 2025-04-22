b23fbbad4f219db0f8feb245fed12146abbcc7c6
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-22 15:26:48 +0200

    Add Moonshot project

---
+++ 36-46 Days - Scaling up to bigger apps/Day 46 - Project 9 (part four)/Moonshot/Moonshot/CrewRoster.swift
@@ -0,0 +1,51 @@
+//
+//  CrewRoster.swift
+//  Moonshot
+//
+//  Created by Enea Xharja on 15.04.25.
+//
+
+import SwiftUI
+
+struct CrewRoster: View {
+    let crew: [MissionView.CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink(value: crewMember.astronaut) {
+                        VStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 144, height: 112)
+                                .clipShape(.circle)
+                                .overlay(
+                                    Circle()
+                                        .strokeBorder(.white, lineWidth: 0.5)
+                                )
+                            
+                            VStack(alignment: .center) {
+                                Text(crewMember.astronaut.name)
+                                    .font(.subheadline)
+                                    .foregroundStyle(.white)
+                                
+                                Text(crewMember.role)
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.75))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+        .navigationDestination(for: Astronaut.self) { astronaut in
+            AstronautView(astronaut: astronaut)
+        }
+    }
+}
+
+#Preview {
+    CrewRoster(crew: [])
+}
---
+++ 36-46 Days - Scaling up to bigger apps/Day 46 - Project 9 (part four)/Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,89 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Enea Xharja on 15.04.25.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    struct CrewMember: Hashable {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .containerRelativeFrame(.horizontal) { width, axis in
+                            width * 0.6
+                        }
+                        .padding(10)
+                }
+                .background(.white)
+                .clipShape(.rect(cornerRadius: 10))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.lightBackground)
+                )
+                
+                if let date = mission.launchDate {
+                    Label(date.formatted(date: .complete, time: .omitted), systemImage: "calendar")
+                        .font(.subheadline)
+                        .padding(.top, 10)
+                }
+                
+                VStack(alignment: .leading) {
+                    CustomDivider()
+                    
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                    
+                    CustomDivider()
+                    
+                    Text("Crew")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                }
+                .padding(.horizontal)
+                
+                CrewRoster(crew: crew)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return MissionView(mission: missions[5], astronauts: astronauts)
+        .preferredColorScheme(.dark)
+}
