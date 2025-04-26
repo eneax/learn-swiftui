a7febdb3aca18a6098128a5642241934c715be84
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-26 21:36:59 +0200

    Add Adding Codable conformance to an @Observable class

---
+++ 49-59 Days - Focus on data/50 Day - Project 10 (part two)/Adding Codable conformance to an @Observable class/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Enea Xharja on 25.04.25.
+//
+
+import SwiftUI
+
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    
+    var name = "Taylor"
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button("Encode Taylor", action: encodeTaylor)
+    }
+    
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
+    }
+}
+
+#Preview {
+    ContentView()
+}
