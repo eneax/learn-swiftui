c861b374759c8928e481bd2fceff8c385ba06997
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-04-30 17:58:40 +0200

    Building a list with @Query

---
+++ 49-59 Days - Focus on data/54 Day - Project 11 (part two)/Building a list with @Query/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Enea Xharja on 30.04.25.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("☹️")
+        case 2:
+            Text("🥱")
+        case 3:
+            Text("🫤")
+        case 4:
+            Text("☺️")
+        default:
+            Text("🤩")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 4)
+}
