e71f967708ec506d85222c0177cb633f4866b45c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-01 13:37:47 +0200

    Deleting from a SwiftData query

---
+++ 49-59 Days - Focus on data/55 Day - Project 11 (part three)/Deleting from a SwiftData query/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,58 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Enea Xharja on 01.05.25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.title)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        
+        let example = Book(title: "Test title", author: "Test author", genre: "Fantasy", review: "Test review", rating: 4)
+        
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to return preview: \(error.localizedDescription)")
+    }
+}
