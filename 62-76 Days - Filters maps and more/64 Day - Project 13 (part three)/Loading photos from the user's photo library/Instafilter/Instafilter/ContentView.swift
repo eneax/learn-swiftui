3f50bf55b8a076c10e24cdcfbcd2e3615a71f372
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-10 14:29:27 +0200

    Loading photos from the user's photo library

---
+++ 62-76 Days - Filters maps and more/64 Day - Project 13 (part three)/Loading photos from the user's photo library/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Enea Xharja on 08.05.25.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ContentView: View {
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
+    var body: some View {
+        VStack {
+            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
+                Label("Select a picture", systemImage: "photo")
+            }
+            
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
