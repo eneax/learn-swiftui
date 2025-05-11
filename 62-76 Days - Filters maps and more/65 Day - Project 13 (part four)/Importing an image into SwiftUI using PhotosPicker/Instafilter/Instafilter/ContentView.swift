933985ac4f77eca4f2745b37c5d80bdb846dc88f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-11 13:13:26 +0200

    Importing an image into SwiftUI using PhotosPicker

---
+++ 62-76 Days - Filters maps and more/65 Day - Project 13 (part four)/Importing an image into SwiftUI using PhotosPicker/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Enea Xharja on 08.05.25.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // share picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            // more code
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
