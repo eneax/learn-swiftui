49aee1209c665f776f67d95b8e57f56db7157cf5
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-15 17:36:12 +0200

    Using Touch ID and Face ID with SwiftUI

---
+++ 62-76 Days - Filters maps and more/69 Day - Project 14 (part two)/Using Touch ID and Face ID with SwiftUI/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Enea Xharja on 10.05.25.
+//
+
+import LocalAuthentication
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
