ad091b35c851afedecd617874bd24c396d57dae7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-16 17:51:57 +0200

    Selecting and editing map annotations

---
+++ 62-76 Days - Filters maps and more/70 Day - Project 14 (part three)/Selecting and editing map annotations/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Enea Xharja on 12.05.25.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 41.9028, longitude: 12.4964), // Italy
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+    )
+    
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
+    
+    var body: some View {
+        MapReader { proxy in // read position on map, not on screen
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            // .onLongPressGesture { selectedPlace = location }
+                            .simultaneousGesture(
+                                LongPressGesture(minimumDuration: 1).onEnded { _ in selectedPlace = location
+                                }
+                            )
+                    }
+                }
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
