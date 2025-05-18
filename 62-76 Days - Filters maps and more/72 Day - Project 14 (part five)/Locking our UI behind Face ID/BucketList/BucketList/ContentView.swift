d3c4453c53d8897bda2066f85a9671a4234c66e4
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-18 10:51:03 +0200

    Locking our UI behind Face ID

---
+++ 62-76 Days - Filters maps and more/72 Day - Project 14 (part five)/Locking our UI behind Face ID/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Enea Xharja on 12.05.25.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 41.9028, longitude: 12.4964), // Italy
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+    )
+    
+    @State private var viewModel = ViewModel()
+    
+    var body: some View {
+        if viewModel.isUnlocked {
+            MapReader { proxy in // read position on map, not on screen
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                            // .onLongPressGesture { selectedPlace = location }
+                                .simultaneousGesture(
+                                    LongPressGesture(minimumDuration: 1).onEnded { _ in viewModel.selectedPlace = location
+                                    }
+                                )
+                        }
+                    }
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace) { place in
+                    EditView(location: place) {
+                        viewModel.update(location: $0)
+                    }
+                }
+            }
+        } else {
+            Button("Unlock Places", action: viewModel.authenticate)
+                .padding()
+                .background(.blue.gradient)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
