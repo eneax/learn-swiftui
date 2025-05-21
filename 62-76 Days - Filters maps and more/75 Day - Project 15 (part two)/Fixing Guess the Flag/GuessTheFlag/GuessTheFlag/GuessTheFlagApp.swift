bdbc375f8a97f09e4582a5cc7d788f7104c39d5c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-21 12:33:50 +0200

    Fixing Guess the Flag

---
+++ 62-76 Days - Filters maps and more/75 Day - Project 15 (part two)/Fixing Guess the Flag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Enea Xharja on 26.03.25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
