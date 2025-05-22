432635ea473b750a6d26c45cb05e8c6b31479867
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-22 12:53:12 +0200

    Add CupcakeCorner project for day 76

---
+++ 62-76 Days - Filters maps and more/76 Day - Project 15 (part three)/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Enea Xharja on 25.04.25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
