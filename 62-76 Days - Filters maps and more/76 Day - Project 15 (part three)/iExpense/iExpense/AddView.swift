38f780363c87f2cc59886fad84d6a26ee812f93a
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-22 12:54:37 +0200

    Add iExpense project for day 76

---
+++ 62-76 Days - Filters maps and more/76 Day - Project 15 (part three)/iExpense/iExpense/AddView.swift
@@ -0,0 +1,50 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Enea Xharja on 13.04.25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    static let types = ["Personal", "Business"]
+    let localCurrency = Locale.current.currency?.identifier ?? "EUR"
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: localCurrency))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    modelContext.insert(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView()
+        .modelContainer(for: ExpenseItem.self)
+}
