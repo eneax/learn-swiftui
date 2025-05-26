1d908f77af82f20bcd0a698b561157eda71120e1
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-05-26 12:27:17 +0200

    Understanding Swift's Result type

---
+++ 79-94 Days - Controlling UI flow/80 Day - Project 16 (part two)/Understanding Swift's Result type/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Enea Xharja on 15.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var output = ""
+    
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings."
+        }
+        
+        let result = await fetchTask.result
+        
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output = "Error: \(error.localizedDescription)"
+//        }
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Err
+            or: \(error.localizedDescription)"
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 79-94 Days - Controlling UI flow/80 Day - Project 16 (part two)/Understanding Swift's Result type/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Enea Xharja on 15.05.25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
