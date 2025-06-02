38cb4084737fa94f4a665bb601ca840d2fc1a8e3
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-02 12:37:42 +0500

    Triggering events repeatedly using a timer

---
+++ 79-94 Days - Controlling UI flow/87 Day - Project 17 (part two)/Triggering events repeatedly using a timer/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Enea Xharja on 19.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
+    var body: some View {
+        Text("Hello, world!")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+                
+                counter += 1
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
