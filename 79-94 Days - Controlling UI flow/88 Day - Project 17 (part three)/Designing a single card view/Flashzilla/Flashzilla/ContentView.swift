b9f10c24e165a240f374d13050b8e9162be64527
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-03 13:28:42 +0500

    Designing a single card view

---
+++ 79-94 Days - Controlling UI flow/88 Day - Project 17 (part three)/Designing a single card view/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Enea Xharja on 19.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView(card: .example)
+    }
+}
+
+#Preview {
+    ContentView()
+}
