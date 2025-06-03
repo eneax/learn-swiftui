aaddc099f073c9747431067ce9301ffcc017ca38
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-03 13:30:47 +0500

    Building a stack of cards

---
+++ 79-94 Days - Controlling UI flow/88 Day - Project 17 (part three)/Building a stack of cards/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Enea Xharja on 19.05.25.
+//
+
+import SwiftUI
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
+struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
+    var body: some View {
+        ZStack { // background
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack { // timer
+                ZStack { // cards
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 79-94 Days - Controlling UI flow/88 Day - Project 17 (part three)/Building a stack of cards/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Enea Xharja on 19.05.25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
