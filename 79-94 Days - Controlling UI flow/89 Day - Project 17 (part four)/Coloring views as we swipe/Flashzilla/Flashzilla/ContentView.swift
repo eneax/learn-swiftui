54a6815548143c50c339756cc1639719c858b38c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-04 20:09:08 +0500

    Coloring views as we swipe

---
+++ 79-94 Days - Controlling UI flow/89 Day - Project 17 (part four)/Coloring views as we swipe/Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Enea Xharja on 19.05.25.
+//
+
+import SwiftUI
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
+struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
+    var body: some View {
+        ZStack { // background
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack { // timer
+                ZStack { // cards
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+            
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+}
+
+#Preview {
+    ContentView()
+}
