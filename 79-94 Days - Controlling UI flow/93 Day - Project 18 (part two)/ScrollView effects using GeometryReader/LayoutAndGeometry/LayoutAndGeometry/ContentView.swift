2e08e8a675d2b3bf6a6bcc5ec80540b7824a06dc
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-08 12:18:59 +0500

    ScrollView effects using GeometryReader

---
+++ 79-94 Days - Controlling UI flow/93 Day - Project 18 (part two)/ScrollView effects using GeometryReader/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Enea Xharja on 21.05.25.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+//
+//    var body: some View {
+//        GeometryReader { fullView in
+//            ScrollView {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { proxy in
+//                        Text("Row #\(index)")
+//                            .font(.title)
+//                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+//                            .rotation3DEffect(
+//                                .degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5,
+//                                axis: (x: 0, y: 1, z: 0)
+//                            )
+//                    }
+//                    .frame(height: 50)
+//                }
+//            }
+//        }
+//    }
+//}
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { proxy in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(
+                                .degrees(
+                                    -proxy.frame(in: .global).minX) / 8,
+                                    axis: (x: 0, y: 1, z: 0)
+                            )
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
