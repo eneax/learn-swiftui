746d5636fde78030c1c6d9aafa6a89230573a3ba
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-08 12:19:59 +0500

    ScrollView effects using visualEffect() and scrollTargetBehavior()

---
+++ 79-94 Days - Controlling UI flow/93 Day - Project 18 (part two)/ScrollView effects using visualEffect() and scrollTargetBehavior()/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Enea Xharja on 21.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    Text("Number \(num)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, proxy in
+                            content
+                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                        }
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+}
+
+#Preview {
+    ContentView()
+}
