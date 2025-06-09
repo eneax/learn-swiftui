ae16ad1c913fbc23eb85dbd3c6614aa807ae2453
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-09 15:03:30 +0500

    Add Layout and geometry app for day 94

---
+++ 79-94 Days - Controlling UI flow/94 Day - Project 18 (part three)/Wrap up/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Enea Xharja on 21.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(hue: min(1, proxy.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                            .scaleEffect(max(0.5, proxy.frame(in: .global).minY / 400))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
