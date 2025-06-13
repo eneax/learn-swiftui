594432c33484db9d2ce8fa441d7f25628a6b20f9
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-13 11:40:38 +0200

    Changing a view’s layout in response to size classes

---
+++ 96-99 Days - One last project/98 Day - Project 19 (part three)/Changing a view’s layout in response to size classes/SnowSeeker/SnowSeeker/ResortView.swift
@@ -0,0 +1,56 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Enea Xharja on 23.05.25.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                HStack {
+                    if horizontalSizeClass == .compact && dynamicTypeSize > .large {
+                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
+                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
+                }
+                .padding(.vertical)
+                .background(.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge) // don't go bigger than 'xxxLarge'
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(resort.facilities, format: .list(type: .and))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    ResortView(resort: .example)
+}
