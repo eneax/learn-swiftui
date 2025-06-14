015ec768030c570e18218a9179b4830f34b54c2f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-14 11:59:42 +0200

    Add SnowSeeker app for day 99

---
+++ 96-99 Days - One last project/99 Day - Project 19 (part four)/Wrap up/SnowSeeker/SnowSeeker/Favorites.swift
@@ -0,0 +1,45 @@
+//
+//  Favorites.swift
+//  SnowSeeker
+//
+//  Created by Enea Xharja on 23.05.25.
+//
+
+import Foundation
+
+@Observable
+class Favorites {
+    private var resorts: Set<String>
+    private let key = "Favorites"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: key) {
+            if let decoded = try? JSONDecoder().decode(Set<String>.self, from: data) {
+                resorts = decoded
+                return
+            }
+        }
+        
+        resorts = []
+    }
+    
+    func contains(_ resort: Resort) -> Bool {
+        resorts.contains(resort.id)
+    }
+    
+    func add(_ resort: Resort) {
+        resorts.insert(resort.id)
+        save()
+    }
+    
+    func remove(_ resort: Resort) {
+        resorts.remove(resort.id)
+        save()
+    }
+    
+    func save() {
+        if let data = try? JSONEncoder().encode(resorts) {
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+}
